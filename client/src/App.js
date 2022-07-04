d9b97938583d8da8b3b5f8c0bd63978df05b31e9
Sarah Alhuriz <sarahalhuriz@Sarahs-MacBook-Pro.local>
2022-07-04 14:42:26 -0600

    Create ChatRoom component and connect to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './index.css';
 
 import Home from './Home/Home';
+import ChatRoom from "./ChatRoom/ChatRoom";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Home/>} />
+        <Route exact path="/:roomId" element={<ChatRoom/>} />
       </Routes>
     </Router>
   );
